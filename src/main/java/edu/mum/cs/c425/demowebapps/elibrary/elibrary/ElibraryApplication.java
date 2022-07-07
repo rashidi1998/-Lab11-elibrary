c4abd30a518aa1f40afd85d5d3741e8c163701ae
Rashid <ismoilovrashid2@gmail.com>
2022-07-07 05:40:09 -0500

    first commit

---
+++ src/main/java/edu/mum/cs/c425/demowebapps/elibrary/elibrary/ElibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.c425.demowebapps.elibrary.elibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElibraryApplication.class, args);
+    }
+
+}
